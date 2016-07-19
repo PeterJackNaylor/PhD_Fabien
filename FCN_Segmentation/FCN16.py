933211809b7ab4b4f8d974ebe5c76d32c3da2df7
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-07-19 18:45:22 +0200

    sntax error

--- FCN_Segmentation/FCN16.py
+++ FCN_Segmentation/FCN16.py
@@ -99,7 +99,7 @@ def fcn(split, data_train, data_test, classifier_name="FCN16",
     if split != "val":
         n.loss = L.SoftmaxWithLoss(n.score, n.label,
                                    loss_param=dict(normalize=False, ignore_label=255))
-        n.acc = L.Accuracy(n.score2, n.label)
+        n.acc = L.Accuracy(n.score, n.label)
     return n.to_proto()
 
 
