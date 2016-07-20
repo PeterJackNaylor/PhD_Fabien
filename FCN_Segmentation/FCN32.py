2f719aa4a0ac3a0d7560b3134637929e813efae7
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-07-20 13:32:50 +0200

    deploy fix

--- FCN_Segmentation/FCN32.py
+++ FCN_Segmentation/FCN32.py
@@ -36,7 +36,7 @@ def fcn(split, data_train, data_test, classifier_name="FCN32",
         n.data, n.label = L.Python(module='dataLayerFCN', layer=pylayer,
                                    ntop=2, param_str=str(pydata_params))
     else:
-        n.data = L.Data()
+        n.data = L.Data(input_param=dict(shape=dict(dim=[1, 3, 512, 512])))
         # the base net
     n.conv1_1, n.relu1_1 = conv_relu(n.data, 64, pad=100)
     n.conv1_2, n.relu1_2 = conv_relu(n.relu1_1, 64)
@@ -78,7 +78,7 @@ def fcn(split, data_train, data_test, classifier_name="FCN32",
     upscore = L.Deconvolution(score_fr,
                               convolution_param=dict(num_output=2, kernel_size=64, stride=32,
                                                      bias_term=False),
-                              weight_filler=dict(type='bilinear')
+                              weight_filler=dict(type='bilinear'),
                               param=[dict(lr_mult=1)])
     n.__setattr__(classifier_name2, upscore)
 
