5cec36e7930c1b996d283d41ae72c4462b927d7e
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-09-07 18:39:06 +0200

    new message

--- FCN_Segmentation/DeconvNet.py
+++ FCN_Segmentation/DeconvNet.py
@@ -74,15 +74,15 @@ def DeconvBnRelu(bottom, nout, ks=3, pad=0, weight_filler=Gaussian_fil, bias_fil
 
 def max_pool(bottom, ks=2, stride=2):
     layer = L.Pooling(bottom, pool=P.Pooling.MAX,
-                      kernel_size=ks, stride=stride, ntop=2)
+                      kernel_size=ks, stride=stride, ntop=3)
     return layer
 
 
-def max_unpool(bottom1, bottom2, unpool_size=14, ks=2, stride=2):
+def max_unpool(bottom1, bottom2, bottom3, unpool_size=14, ks=2, stride=2):
     unpooling_param = dict(pool=P.Pooling.MAX, kernel_size=ks,
                            stride=stride)
     # should be unpooling?
-    return L.Unpooling(bottom1, bottom2, pooling_param=unpooling_param)
+    return L.Unpooling(bottom1, bottom2, bottom3, pooling_param=unpooling_param)
 
 
 def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
@@ -102,26 +102,26 @@ def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
     n.conv2_1, n.bn2_1, n.relu2_1 = ConvBnRelu(n.pool1, 128, 3, 1, 1)
     n.conv2_2, n.bn2_2, n.relu2_2 = ConvBnRelu(n.relu2_1, 128, 3, 1, 1)
 
-    n.pool2, n.pool2_mask = max_pool(n.relu2_2, ks=2, stride=2)
+    n.pool2, n.pool2_mask, n.pool2_count = max_pool(n.relu2_2, ks=2, stride=2)
 
     n.conv3_1, n.bn3_1, n.relu3_1 = ConvBnRelu(n.pool2, 256, 3, 1, 1)
     n.conv3_2, n.bn3_2, n.relu3_2 = ConvBnRelu(n.relu3_1, 256, 3, 1, 1)
     n.conv3_3, n.bn3_3, n.relu3_3 = ConvBnRelu(n.relu3_2, 256, 3, 1, 1)
 
     pdb.set_trace()
-    n.pool3, n.pool3_mask = max_pool(n.relu3_3, ks=2, stride=2)
+    n.pool3, n.pool3_mask, n.pool3_count = max_pool(n.relu3_3, ks=2, stride=2)
 
     n.conv4_1, n.bn4_1, n.relu4_1 = ConvBnRelu(n.pool3, 512, 3, 1, 1)
     n.conv4_2, n.bn4_2, n.relu4_2 = ConvBnRelu(n.relu4_1, 512, 3, 1, 1)
     n.conv4_3, n.bn4_3, n.relu4_3 = ConvBnRelu(n.relu4_2, 512, 3, 1, 1)
 
-    n.pool4, n.pool4_mask = max_pool(n.relu4_3, ks=2, stride=2)
+    n.pool4, n.pool4_mask, n.pool4_count = max_pool(n.relu4_3, ks=2, stride=2)
 
     n.conv5_1, n.bn5_1, n.relu5_1 = ConvBnRelu(n.pool4, 512, 3, 1, 1)
     n.conv5_2, n.bn5_2, n.relu5_2 = ConvBnRelu(n.relu5_1, 512, 3, 1, 1)
     n.conv5_3, n.bn5_3, n.relu5_3 = ConvBnRelu(n.relu5_2, 512, 3, 1, 1)
 
-    n.pool5, n.pool5_mask = max_pool(n.relu5_3, ks=2, stride=2)
+    n.pool5, n.pool5_mask, n.pool5_count = max_pool(n.relu5_3, ks=2, stride=2)
 
     n.fc6, n.bn6, n.relu6 = ConvBnRelu(n.pool5, 4096, ks=7, stride=1, pad=0)
     #n.drop6 = L.Dropout(n.relu6, dropout_ratio=0.5, in_place=True)
@@ -132,7 +132,8 @@ def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
     n.deconv_fc6, n.deconv_fc6_bn, n.deconv_fc6_relu = DeconvBnRelu(
         n.relu7, 512, ks=7)
 
-    n.unpool5 = max_unpool(n.deconv_fc6_relu, n.pool5_mask, unpool_size=14)
+    n.unpool5 = max_unpool(n.deconv_fc6_relu, n.pool5_mask,
+                           n.pool5_count, unpool_size=14)
 
     n.deconv5_1, n.debn5_1, n.derelu5_1 = DeconvBnRelu(
         n.unpool5, 512, ks=3, pad=1)
@@ -141,7 +142,8 @@ def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
     n.deconv5_3, n.debn5_3, n.derelu5_3 = DeconvBnRelu(
         n.derelu5_2, 512, ks=3, pad=1)
 
-    n.unpool4 = max_unpool(n.derelu5_3, n.pool4_mask, unpool_size=28)
+    n.unpool4 = max_unpool(n.derelu5_3, n.pool4_mask,
+                           n.pool4_count, unpool_size=28)
 
     n.deconv4_1, n.debn4_1, n.derelu4_1 = DeconvBnRelu(
         n.unpool4, 512, ks=3, pad=1)
@@ -150,7 +152,8 @@ def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
     n.deconv4_3, n.debn4_3, n.derelu4_3 = DeconvBnRelu(
         n.derelu4_2, 256, ks=3, pad=1)
 
-    n.unpool3 = max_unpool(n.derelu4_3, n.pool3_mask, unpool_size=56)
+    n.unpool3 = max_unpool(n.derelu4_3, n.pool3_mask,
+                           n.pool3_count, unpool_size=56)
 
     n.deconv3_1, n.debn3_1, n.derelu3_1 = DeconvBnRelu(
         n.unpool3, 256, ks=3, pad=1)
@@ -159,14 +162,16 @@ def DeconvNet(split, data_gene, classifier_name="DeconvNet"):
     n.deconv3_3, n.debn3_3, n.derelu3_3 = DeconvBnRelu(
         n.derelu3_2, 128, ks=3, pad=1)
 
-    n.unpool2 = max_unpool(n.derelu3_3, n.pool2_mask, unpool_size=112)
+    n.unpool2 = max_unpool(n.derelu3_3, n.pool2_mask,
+                           n.pool2_count, unpool_size=112)
 
     n.deconv2_1, n.debn2_1, n.derelu2_1 = DeconvBnRelu(
         n.unpool2, 128, ks=3, pad=1)
     n.deconv2_2, n.debn2_2, n.derelu2_2 = DeconvBnRelu(
         n.derelu2_1, 64, ks=3, pad=1)
 
-    n.unpool1 = max_unpool(n.derelu2_2, n.pool1_mask, unpool_size=224)
+    n.unpool1 = max_unpool(n.derelu2_2, n.pool1_mask,
+                           n.pool1_count, unpool_size=224)
 
     n.deconv1_1, n.debn1_1, n.derelu1_1 = DeconvBnRelu(
         n.unpool1, 64, ks=3, pad=1)
