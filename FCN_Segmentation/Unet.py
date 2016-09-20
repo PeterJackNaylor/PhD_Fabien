f1a2cc1062aea5e2a8694a88a3bf64191ebd3550
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-09-20 15:11:33 +0200

    too many parameters for deconv

--- FCN_Segmentation/Unet.py
+++ FCN_Segmentation/Unet.py
@@ -54,9 +54,9 @@ def DeconvReCropConcatConvReConvRe(bottom1, bridge2, val, deconv_out=None):
     deconv = L.Deconvolution(bottom1,
                              convolution_param=dict(num_output=deconv_out, kernel_size=2, stride=2,
                                                     bias_term=False, weight_filler=dict(type="xavier")),
-                             param=[dict(lr_mult=1, decay_mult=1),
-                                    dict(lr_mult=2, decay_mult=0)]
-                             )
+                             param=[dict(lr_mult=1)])  # , decay_mult=1),
+    #       dict(lr_mult=2, decay_mult=0)]
+    #)
     relu1 = Relu(deconv)
     croped = crop(bridge2, relu1)
     concat = L.Concat(relu1, croped)
