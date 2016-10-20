f9ac4610f246e2284f1f515670a29791e7761e6e
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-10-20 09:41:40 +0200

    Baochuan

--- Baochuan/WriteBaochuanNet.py
+++ Baochuan/WriteBaochuanNet.py
@@ -16,7 +16,6 @@ def WriteBaochuanNet(kwargs):
         cn : classifier name
         batch_size: batch size for trai 
     """
-    pdb.set_trace()
     batch_size = kwargs['batch_size']
 
     path_ = os.path.join(kwargs['wd'], kwargs['cn'])
