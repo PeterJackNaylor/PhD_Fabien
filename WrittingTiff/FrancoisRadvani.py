62dc77cbb8c897738bb3793a3aa97cbd69116729
git <peter.naylor@mines-paristech.fr>
2017-05-22 11:53:16 +0200

    clear border option

--- WrittingTiff/FrancoisRadvani.py
+++ WrittingTiff/FrancoisRadvani.py
@@ -8,7 +8,7 @@ import pdb
 from Deprocessing.Transfer import ChangeEnv
 import progressbar
 from scipy.ndimage.morphology import morphological_gradient
-
+from skimage.segmentation import clear_border
 
 
 
@@ -41,53 +41,46 @@ def sliding_window(image, stepSize, windowSize):
             yield (x, y, x + windowSize[0], y + windowSize[1], res_img)
 
 
-def PredLargeImageFromNet(net_1, image, stepSize, windowSize, removeFromBorder = 10, method="avg"):
+def PredLargeImageFromNet(net_1, image, stepSize, windowSize, removeFromBorder = 10, method="avg", ClearBorder = True):
     #pdb.set_trace() 
-    if method == "avg":
-        x_s, y_s, z_s = image.shape
-        result = np.zeros(shape=(x_s, y_s, 2))
-        for x_b, y_b, x_e, y_e, window in sliding_window(image, stepSize, windowSize):
-            prob_image1, bin_image1 = PredImageFromNet(net_1, window, with_depross=True)
-            val = removeFromBorder
-            y_b += val
-            y_e -= val
-            x_b += val
-            x_e -= val
-            if val != 0:
-                result[y_b:y_e, x_b:x_e, 0] += prob_image1[val:-val, val:-val]
-                #imsave("Slide/{}_{}_{}_{}.png".format(y_b,y_e,x_b,x_e), prob_image1)
-            else:
-                result[y_b:y_e, x_b:x_e, 0] += prob_image1
-            
+    x_s, y_s, z_s = image.shape
+    result = np.zeros(shape=(x_s, y_s, 2))
+    for x_b, y_b, x_e, y_e, window in sliding_window(image, stepSize, windowSize):
+        prob_image1, bin_image1 = PredImageFromNet(net_1, window, with_depross=True)
+        val = removeFromBorder
+        y_b += val
+        y_e -= val
+        x_b += val
+        x_e -= val
+
+
+        inter_result = prob_image1[val:-val, val:-val] if val!= 0 else prob_image1
+
+        if ClearBorder:
+            inter_bin = (inter_result > 0.5 + 0.0).astype(np.uint8)
+            inter_bin = clear_border(inter_bin)
+            inter_result[inter_bin == 0] = 0
+
+        if method == 'avg':
+            result[y_b:y_e, x_b:x_e, 0] += inter_result
             result[y_b:y_e, x_b:x_e, 1] += 1.
+
+        elif method == "max":
+            result[y_b:y_e, x_b:x_e, 1] = inter_result
+            result[y_b:y_e, x_b:x_e, 0] = np.max(result[y_b:y_e, x_b:x_e,:], axis=2)
+
+    if method == "avg" :
         x, y = np.where(result[:,:,1] == 0)
         result[x, y, 1] = 1
         prob_map = result[:, :, 0] / result[:, :, 1]
-        bin_map = prob_map > 0.5 + 0.0
-        bin_map = bin_map.astype(np.uint8)
+        
     elif method == "max":
-        x_s, y_s, z_s = image.shape
-        result = np.zeros(shape=(x_s, y_s, 2))
-        for x_b, y_b, x_e, y_e, window in sliding_window(image, stepSize, windowSize):
-            prob_image1, bin_image1 = PredImageFromNet(net_1, window, with_depross=True)
-            val = removeFromBorder
-            y_b += val
-            y_e -= val
-            x_b += val
-            x_e -= val
-	    # pdb.set_trace()
-            if val != 0:
-                result[y_b:y_e, x_b:x_e, 1] = prob_image1[val:-val, val:-val]
-                #imsave("Slide/{}_{}_{}_{}.png".format(y_b,y_e,x_b,x_e), prob_image1)
-            else:
-                result[y_b:y_e, x_b:x_e, 1] = prob_image1
-            result[y_b:y_e, x_b:x_e, 0] = np.max(result[y_b:y_e, x_b:x_e,:], axis=2)
-
         prob_map = result[:, :, 0].copy()
-        bin_map = prob_map > 0.5 + 0.0
-        bin_map = bin_map.astype(np.uint8)
 
+    bin_map = prob_map > 0.5 + 0.0
+    bin_map = bin_map.astype(np.uint8)
     return prob_map, bin_map
+
 
 def pred_f(image, net1, net2, stepSize=stepSize, windowSize=windowSize, param=param, border=10, method="avg"):
     prob_image1, bin_image1 = PredLargeImageFromNet(net1, image, stepSize, windowSize, border, method)
@@ -108,15 +101,15 @@ def PredOneImage(path, outfile, c, f, net1, net2):
         image = imread(path)[:,:,0:3]
         image = c(image)
         prob = f(image, net1, net2, method="avg")
-	imsave(outfile.replace('.png','_raw.png'), image)
+        imsave(outfile.replace('.png','_raw.png'), image)
         imsave(outfile.replace('.png', '_prob.png'), prob)
-        for param in range(6,35,2):
+        for param in range(6,10,2):
             image_seg = PostProcess(prob,param)
-	    ContourSegmentation = Contours(image_seg)
-    	    x_, y_ = np.where(ContourSegmentation > 0)
-	    image_segmented = image.copy()
-	    image_segmented[x_,y_,:] = np.array([0,0,0])
-	    imsave(outfile.replace('.png', '_wsl_{}.png').format(param), image_segmented)
+            ContourSegmentation = Contours(image_seg)
+            x_, y_ = np.where(ContourSegmentation > 0)
+            image_segmented = image.copy()
+            image_segmented[x_,y_,:] = np.array([0,0,0])
+            imsave(outfile.replace('.png', '_wsl_{}.png').format(param), image_segmented)
     else:
         #print "Files exists"
         pass
@@ -128,7 +121,7 @@ def crop(image):
 if __name__ == "__main__":
     
     PATH = "/data/users/pnaylor/Documents/Python/Francois/Only_one_images/*40x.png"
-    OUT = "/data/users/pnaylor/Documents/Python/Francois/AVG"
+    OUT = "/data/users/pnaylor/Documents/Python/Francois/ClearBorders"
 
 
 
