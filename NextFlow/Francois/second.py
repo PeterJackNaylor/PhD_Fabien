f14fbab5d89170e7270f57ff38d909ed0becc1c0
Peter Naylor <peter.naylor@mines-paristech.fr>
2017-05-29 19:12:57 +0200

    nextflow francois update

---
+++ NextFlow/Francois/second.py
@@ -0,0 +1,38 @@
+from os.path import join
+from scipy.misc import imread
+from WrittingTiff.Extractors import bin_analyser
+
+def file_retriever(filename):
+    bin_image = join(filename, filename + "_bin.png")
+    raw_image = join(filename, filename + "_RAW.png")
+    table_n = join(filename, filename + "_table.csv")
+    return imread(raw_image), imread(bin_image)
+
+
+
+
+def Extractors():
+
+
+
+    return list_extract
+
+
+if __name__ == "__main__":
+    
+
+    parser = OptionParser()
+
+    parser.add_option("--folder_name", dest="folder_name",
+                      help="Input folder (raw/bin/seg/prob data)")
+
+    (options, args) = parser.parse_args()
+
+    img, bin = file_retriever(options.folder_name)
+
+    list_extractors = Extractors()
+
+    table = bin_analyser(img, bin, list_extractors, pandas_table=True)
+    table_name = 
+    ### write bit to save table in folder
+    
