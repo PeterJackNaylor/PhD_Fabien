26f2379bec246f58ffcb65634291ee60f3f40e13
pnaylor <peter.naylor@mines-paristech.fr>
2017-09-20 17:08:58 +0200

    update

--- NextFlow/FCN/GivingBackID.py
+++ NextFlow/FCN/GivingBackID.py
@@ -4,9 +4,11 @@ import os
 
 
 def ChangeDirrectory(foldername, old_id, new_id):
-    for subfile in glob(foldername + "/*.png"):
-        os.rename(subfile, subfile.replace(old_id, new_id))
     os.rename(foldername, foldername.replace(old_id, new_id))
+    files_to_change = glob(foldername.replace(old_id, new_id) + "/*.png")
+    for subfile in files_to_change:
+        os.rename(subfile, subfile.replace("/" + old_id + "_", "/" + new_id + "_"))
+    
 
 
 Mapping = {}
@@ -27,4 +29,3 @@ for id_key in Mapping.keys():
     val = Mapping[id_key]
     ChangeDirrectory('./ToAnnotateColor/Slide_{}'.format(val), val, id_key)
     ChangeDirrectory('./ToAnnotateColor/GT_{}'.format(val), val, id_key)
-
