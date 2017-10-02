2bad1e10b2e87a0807335a3a8c25652116c0a91f
pnaylor <peter.naylor@mines-paristech.fr>
2017-10-02 14:45:21 +0200

    update

--- NextFlow/Bruno/GroundStateWithLHYTermSingleComponent_SolitonDroplet.m
+++ NextFlow/Bruno/GroundStateWithLHYTermSingleComponent_SolitonDroplet.m
@@ -1,4 +1,4 @@
-function s=GroundStateWithLHYTermSingleComponent_SolitonDroplet(bpointdiagram, begining, ending)
+function GroundStateWithLHYTermSingleComponent_SolitonDroplet(bpointdiagram, begining, ending)
 
 disp(begining)
 disp(ending)
@@ -75,151 +75,112 @@ BVec=[BminLoop:DBLoop:BMaxLoop];
 
 %% get scattering from file
 SimoniData = importdata(DATFILE);
-BSimoni=SimoniData(:,1);
-abbSimoni=SimoniData(:,2);
-accSimoni=SimoniData(:,3);
-abcSimoni=SimoniData(:,4);
+BSimoni=SimoniData(:, 1);
+abbSimoni=SimoniData(:, 2);
+accSimoni=SimoniData(:, 3);
+abcSimoni=SimoniData(:, 4);
 
-abb = spline(BSimoni,abbSimoni,Bgrid)*a0;
-abc = spline(BSimoni,abcSimoni,Bgrid)*a0;
-acc = spline(BSimoni,accSimoni,Bgrid)*a0;
-
-
-trapFrequencyRadial=109; %in Hz
-trapFrequencyAxial=5; % in Hz (you have to put a non-zero frequency)
+abb = spline(BSimoni, abbSimoni, Bgrid) * a0;
+abc = spline(BSimoni, abcSimoni, Bgrid) * a0;
+acc = spline(BSimoni, accSimoni, Bgrid) * a0;
 
 
+trapFrequencyRadial = 109; %in Hz
+trapFrequencyAxial = 5; % in Hz (you have to put a non-zero frequency)
 
-parfor j=begining:ending
+
+
+parfor j = begining:ending
 % B=56.1;   
-B=BminLoop+(j-1)*DBLoop;
+B = BminLoop+(j-1)*DBLoop;
 [tt I]=min((B-Bgrid).^2);
-a1=abb(I);
-a2=acc(I),
-a12=abc(I);
+a1 = abb(I);
+a2 = acc(I),
+a12 = abc(I);
 
 temp_nmax_j = zeros(Npoint)
-temp_com = zeros(Npoint)
-temp_variance = zeros(Npoint)
 
-for i=Npoint:-1:1
+for i = Npoint:-1:1
 
-N=Nat(i);   % Total number of atoms
+N = Nat(i);   % Total number of atoms
 
-% a1=abb(i);
-% a2=acc(i),
-% a12=abc(i);
 
 % 
-Nm=2000+5500*(B-55.2).^2;
-if N>Nm
-Deltat=0.5e-3
+Nm = 2000 + 5500 * (B - 55.2).^2;
+if N > Nm
+Deltat = 0.5e-3
 else
-Deltat=0.2e-2
-end
+Deltat = 0.2e-2
+end %end if
 Method = Method_Var3d(Computation, Ncomponents, Type, Deltat, Stop_time, Stop_crit);
 %% calculation adimensioned parameters
 
 %trap
-omegam=2*pi*trapFrequencyAxial;
-aHO=sqrt(hbar/m/2/pi/trapFrequencyAxial);
-aHORadial=sqrt(hbar/m/2/pi/trapFrequencyRadial);
+omegam = 2 * pi * trapFrequencyAxial;
+aHO = sqrt(hbar / m / 2 / pi / trapFrequencyAxial);
+aHORadial = sqrt(hbar / m / 2 / pi / trapFrequencyRadial);
 
-gamma_x=1;
-gamma_y=trapFrequencyRadial/trapFrequencyAxial;
-gamma_z=trapFrequencyRadial/trapFrequencyAxial;
+gamma_x = 1;
+gamma_y = trapFrequencyRadial / trapFrequencyAxial;
+gamma_z = trapFrequencyRadial / trapFrequencyAxial;
 
 
 
 % interactions
-g1=a1*4*pi*hbar^2/m;g2=a2*4*pi*hbar^2/m;g12=a12*4*pi*hbar^2/m;
-deltaa=a12+sqrt(a1*a2);
-deltag=deltaa*4*pi*hbar^2/m;
-Alpha=(1+sqrt(g1/g2))/2;
-x=g12^2/g1/g2;
-y=sqrt(g2/g1);
-F=((1+y+sqrt((1-y)^2+4*x*y))^(5/2)+(abs(1+y-sqrt((1-y)^2+4*x*y)))^(5/2))/4/sqrt(2); %Petrov function
+g1 = a1 * 4 * pi * hbar^2 / m ; g2 = a2 * 4 * pi * hbar^2 / m ; g12 = a12 * 4 * pi * hbar^2 / m;
+deltaa = a12 + sqrt(a1 * a2);
+deltag = deltaa * 4 * pi * hbar^2 / m;
+Alpha = (1 + sqrt(g1 / g2)) / 2;
+x = g12^2 / g1 / g2;
+y = sqrt(g2 / g1);
+F = ((1 + y + sqrt(( 1 - y )^2 + 4 * x * y ))^(5 / 2) + (abs( 1 + y - sqrt(( 1 - y )^2 + 4 * x * y)))^(5 / 2)) / 4 / sqrt(2); %Petrov function
 
-n0=N/(2*Alpha*aHO^3);
+n0 = N / (2 * Alpha * aHO^3);
 %% GP adimensioned parameters
-beta=(2*Alpha-1)/(2*Alpha^2)*deltag/aHO^3/hbar/omegam*N; %mean field prefactor
+beta = (2 * Alpha - 1) / (2 * Alpha^2) * deltag / aHO^3 / hbar / omegam * N; %mean field prefactor
 
-gamma=8*F/(3*sqrt(2*pi)*Alpha^(5/2))*(g1/(aHO^3*hbar*omegam))*(a1/aHO)^(3/2)*N^(3/2); %LHY prefactor
+gamma = 8 * F / (3 * sqrt(2 * pi) * Alpha^(5 / 2)) * (g1 / (aHO^3 * hbar * omegam)) * (a1 / aHO)^(3 / 2) * N^(3 / 2); %LHY prefactor
 
 %%% 
-Physics3D = Physics3D_Var3d(Method, 0.5,1,0);
+Physics3D = Physics3D_Var3d(Method, 0.5, 1, 0);
 Physics3D = Dispersion_Var3d(Method, Physics3D);
-Physics3D = Potential_Var3d(Method, Physics3D,@(X,Y,Z) quadratic_potential3d(gamma_x,gamma_y,gamma_z,X,Y,Z));
-Physics3D = Nonlinearity_Var3d(Method, Physics3D,@(phi,x,y,z) beta*abs(phi).^2+gamma*abs(phi).^(3));
+Physics3D = Potential_Var3d(Method, Physics3D, @(X, Y, Z) quadratic_potential3d(gamma_x, gamma_y, gamma_z, X, Y, Z));
+Physics3D = Nonlinearity_Var3d(Method, Physics3D, @(phi, x, y, z) beta * abs(phi).^2 + gamma * abs(phi).^(3));
 Physics3D = Gradientx_Var3d(Method, Physics3D);
 Physics3D = Gradienty_Var3d(Method, Physics3D);
 
 %% Setting the initial data
 InitialData_Choice = 1;
-if (i==Npoint)
+if (i == Npoint)
 
 Phi_0 = InitialData_Var3d(Method, Geometry3D, Physics3D, InitialData_Choice);
-%Phi_U=load(horzcat('D:\GPEWavefunctions\Phi_initial.mat'));
-%Phi_0=Phi_U.Phi_0;  
+
 else     
-Phi_U=load(horzcat(SAVELOCATION, num2str(Nat(i+1)),'B_',num2str(B),'G.mat'));
-Phi_0=Phi_U.Phi;  
-end
-%% Phi_U=load('C:\Users\pcheiney\Desktop\PhiDroplet');
-%% Phi_0=Phi_U.Phi;
-%% Setting informations and outputs
+Phi_U = load(horzcat(SAVELOCATION, num2str(Nat(i+1)),'B_',num2str(B),'G.mat'));
+Phi_0 = Phi_U.Phi;  
+end % end if
+
 Outputs = OutputsINI_Var3d(Method);
 Printing = 1;
 Evo = 15;
 Draw = 0;
-Print = Print_Var2d(Printing,Evo,Draw);
+Print = Print_Var2d(Printing, Evo, Draw);
+
 %-----------------------------------------------------------
 % Launching simulation
 %-----------------------------------------------------------
 
-[Phi, Outputs] = GPELab3d(Phi_0,Method,Geometry3D,Physics3D,Outputs,[],Print);
-%Phisave(i)=Phi;
+[Phi, Outputs] = GPELab3d(Phi_0, Method, Geometry3D, Physics3D, Outputs, [], Print);
 
-%%%save(horzcat('D:\GPEWavefunctions\Phi_Up_N_',num2str(Nat(i)),'B_',num2str(B),'G.mat'),'Phi');
+save(horzcat(SAVELOCATION, num2str(Nat(i)), 'B_',num2str(B), 'G.mat'), 'Phi');
 
-save(horzcat(SAVELOCATION, num2str(Nat(i)),'B_',num2str(B),'G.mat'),'Phi');
-%save(horzcat('Y:\Theory\Cleaned Matlab script\Cleaned Matlab script\Test\GPEWavefunctions\Phi_Up_N_',num2str(Nat),'B_',num2str(B),'G.mat'),'Phi');
+temp_nmax_j(i) =  2 * Alpha * n0 * max( max( max( abs( Phi{1} ).^2 ) ) ) 
 
-%% Ploting ouptut
-
-%%grid in um
-% X=[0:Nx-1]/(Nx-1)*(xmax-xmin)+xmin;X=X*aHO;
-% Y=[0:Ny-1]/(Ny-1)*(ymax-ymin)+ymin;Y=Y*aHO;
-% Z=[0:Nz-1]/(Nz-1)*(zmax-zmin)+zmin;Z=Z*aHO;
-%%ThomasFermi profile
-
-% nTFmax=n0*0.5*(15/4/pi)^(2/5)*beta^(-3/5)*(gamma_x*gamma_y*gamma_z)^(2/5)
-% nTFX=max(0,nTFmax-0.5*n0*gamma_x^2/beta*(X/aHO).^2);
-% nTFY=max(0,nTFmax-0.5*n0*gamma_y^2/beta*(Y/aHO).^2);
-% nTFZ=max(0,nTFmax-0.5*n0*gamma_z^2/beta*(Z/aHO).^2);
-
-% dx=(xmax-xmin)/(Nx-1);
-% dy=(ymax-ymin)/(Ny-1);
-% dz=(zmax-zmin)/(Nz-1);
-
-%% calulate moments
-% [meshX,meshY,meshZ] = meshgrid(X,Y,Z);
-temp_nmax_j(i) =  2*Alpha*n0*max(max(max(abs(Phi{1}).^2)))
-% temp_com(i).x=sum(sum(sum(meshX.*abs(Phi{1}).^2)));
-% temp_com(i).y=sum(sum(sum(meshX.*abs(Phi{1}).^2)));
-% temp_com(i).z=sum(sum(sum(meshX.*abs(Phi{1}).^2)));
-
-% temp_variance(i).x=sum(sum(sum((meshX-temp_com(i).x).^2.*abs(Phi{1}).^2)));
-% temp_variance(i).y=sum(sum(sum((meshY-temp_com(i).y).^2.*abs(Phi{1}).^2)));
-% temp_variance(i).z=sum(sum(sum((meshZ-temp_com(i).z).^2.*abs(Phi{1}).^2)));
-end
-% COM = temp_com
-% variance = temp_variance
+end %end i foor loop
 nmax(j,:) = temp_nmax_j
-end
+end %end j parfor loop
 
 save(horzcat(SAVEMAT),'nmax');
 save(horzcat(SAVEMAT_Nat), 'Nat');
 save(horzcat(SAVEMAT_BVec), 'BVec');
-s=1
-end
+end % end function
